b1159f39c3caee6504258961c82fe4b20fd8801d
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-03-04 15:58:50 +0900

    init: project initialize

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,30 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+
+            //em.persist(member);
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+        emf.close();
+    }
+}
