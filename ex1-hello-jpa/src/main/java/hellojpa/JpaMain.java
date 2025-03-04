134b2c2056449c8920e33479aec4f469a51dbc1b
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-03-04 17:15:39 +0900

    add: JPQL test

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -14,8 +16,14 @@ public class JpaMain {
 
         try{
 
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+            //Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
 
             //em.persist(member);
