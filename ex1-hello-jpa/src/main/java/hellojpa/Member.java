4fe9f8e364d8933edd64ba00dba0d323fe82262d
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-03-10 17:21:43 +0900

    add: GeneratedValue

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,13 +1,17 @@
 package hellojpa;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class Member {
 
     @Id
+    @GeneratedValue
     private Long id;
+
+
     private String name;
 
     public Member(){
